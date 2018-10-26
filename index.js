6551c3d6a2598e8187e7ffdb6db3907a75f26380
StylLpl <loclion17@gmail.com>
2018-10-26 23:26:57 +0700

    a

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views'. './views');
+
+
+
 app.get('/', function (rep, res) {
     res.send('Hello. Lude');
 })
 
 app.listen(port, function(){
-    console.log("Hello");
+    console.log("Server listening on port " + port);
 });
